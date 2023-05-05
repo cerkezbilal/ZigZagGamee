c6701ef77bd6f72fb2b7debfed2814bc94071fdf
Bilal Karademir <34405358+cerkezbilal@users.noreply.github.com>
2023-05-05 15:51:44 +0300

    Panel Desing created

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,22 +6,24 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
 
-    Vector3 yon = Vector3.left;
-
+    [Header("Out Component")]
     [SerializeField] float speed;
-    int bestScore = 0;
-
-    public GroundSpawner groundSpawner;
-
-    public static bool isDead = false;
-
-    public float hizlanmaZorlugu;
-
-    float score = 0f;
-    float artisMiktari = 1f;
-
     [SerializeField]
     Text scoreText, bestScoreText;
+    [SerializeField]
+    GameObject restartPanel, playGamePanel;
+
+    [Header("Public Variable")]
+    public GroundSpawner groundSpawner;
+    public static bool isDead = false;
+    public float hizlanmaZorlugu;
+
+
+   
+    Vector3 yon = Vector3.left;
+    int bestScore = 0;
+    float score = 0f;
+    float artisMiktari = 1f;
 
     private void Start()
     {
@@ -87,7 +89,7 @@ public class PlayerController : MonoBehaviour
             groundSpawner.ZeminOlustur();
         }
     }
-
+    
     IEnumerator YokEt(GameObject zemin)
     {
 
