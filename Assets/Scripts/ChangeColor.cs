a7b8a0b0f90c0b140ce443adb74f9fedae88b93c
Bilal Karademir <34405358+cerkezbilal@users.noreply.github.com>
2023-05-05 15:25:11 +0300

    Color changed end

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -14,8 +14,37 @@ public class ChangeColor : MonoBehaviour
     private void Start()
     {
         birinciRenk = Random.Range(0, colors.Length);
-        zeminMat.color = colors[birinciRenk];
-        Camera.main.backgroundColor = colors[birinciRenk];
+        ikinciRenk = colors[IkinciRenkSec()];
+        ilkRenk = colors[birinciRenk];
+        zeminMat.color = ilkRenk;
+        Camera.main.backgroundColor = ilkRenk;
+    }
+
+
+    private void Update()
+    {
+        Color fark = ilkRenk - ikinciRenk;
+
+        if(Mathf.Abs(fark.r) + Mathf.Abs(fark.g) + Mathf.Abs(fark.b) < 0.2f)
+        {
+            ikinciRenk = colors[IkinciRenkSec()];
+        }
+        zeminMat.color = Color.Lerp(zeminMat.color, ikinciRenk,0.003f);
+        Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor, ikinciRenk, 0.0007f);
+    }
+
+
+    int IkinciRenkSec()
+    {
+        int ikinciRenkIndex;
+        ikinciRenkIndex = Random.Range(0, colors.Length);
+
+        while (ikinciRenkIndex == birinciRenk)
+        {
+            ikinciRenkIndex = Random.Range(0, colors.Length);
+        }
+
+        return ikinciRenkIndex;
     }
 
 
